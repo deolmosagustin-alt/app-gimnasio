3f1050b4725149f8cfddfa6c50c8fa1ce370eedd
deolmosagustin-alt <deolmosagustin@gmail.com>
2026-06-23 11:21:50 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000, // <--- Agregá esta línea
+    // Aumentamos el límite para que no rompa el despliegue
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        // Esto ayuda a que el despliegue sea más estable
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   }
 })
